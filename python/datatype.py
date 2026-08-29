d44970cbda1b5a360b4a0b88665f264d64aac4d5
thinlimbscoding-maker <thinlimbscoding@gmail.com>
2026-08-29 20:40:16 +0530

    python basics

---
+++ python/datatype.py
@@ -0,0 +1,29 @@
+a=[1,2,3,4,"121212"]
+print(a)
+
+
+# tupple
+
+tuppleb=(1,2,3,2,2,2,2,2,2,"$")
+tuppleb,b1,*others,last=tuppleb
+
+print(tuppleb)
+# h,*h=tuppleb
+# print(h)
+# here python read like quran right to left so  ....=tuppleb
+
+# now dictory is lik obect 
+
+adic={
+    "name":"kunal",
+    "age":12,
+    "balue":"wew",
+    "asas":'true'
+}
+# print(adic)
+
+
+for key,value in adic.items():
+    print(key)
+    
+
---
+++ python/first.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+Name = "John"
+name="John"
+age=20
+num=102
+print(Name)
+print(name)
+print(Name==name)
+a=int(age)
+
+if age>18:
+    print("you are an adult")
+else:
+    print("you are not an adult")
+multiple=1+2*3-4/5**6+\
+    3*2/4+5-6*7/8**9
+print(multiple)    
+value= str(num)
+print(type(value))
