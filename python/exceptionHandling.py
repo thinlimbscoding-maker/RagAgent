5cd3cff79b917b5c2653c17004c3e21049c4a9ea
thinlimbscoding-maker <thinlimbscoding@gmail.com>
2026-08-30 17:26:34 +0530

    exception handing

---
+++ python/exceptionHandling.py
@@ -0,0 +1,13 @@
+try:
+    a = 10
+
+except:
+    print("plz enter a proper input")
+else:
+    if a < 18:
+        a / 10
+    else:
+        print("mr kunal is innocent")
+
+finally:
+    print("all opreation happiend")
